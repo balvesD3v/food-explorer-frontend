83d465b4fa0e4f136805f8cd13ec522b3085e48b
balvesD3v <lpbalves7filho@gmail.com>
2023-12-18 19:15:09 -0300

    .

--- src/pages/NewDish/index.jsx
+++ src/pages/NewDish/index.jsx
@@ -1,6 +1,6 @@
 import { DivStyled, InputField, IngredientsField, TextArea } from "./styles";
 import { Header } from "../../components/Header";
-import Footer from "../../components/Footer";
+import { Footer } from "../../components/Footer";
 import { FaAngleLeft } from "react-icons/fa6";
 import { SendImage } from "../../components/SendImage";
 import { InputName } from "../../components/InputName";
