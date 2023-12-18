10202ceb01ac64d69c4432183a99006021b5e9fb
balvesD3v <lpbalves7filho@gmail.com>
2023-12-18 19:24:19 -0300

    .

--- src/pages/ShowDish/index.jsx
+++ src/pages/ShowDish/index.jsx
@@ -1,5 +1,5 @@
 import { Header } from "../../components/Header";
-import { Footer } from "../../components/Footer";
+import { Footer } from "../../components/Footer/index";
 import { Button } from "../../components/Button";
 import { Tags } from "../../components/Tags";
 import { DivContent, ContentStyled, IngredientsStyled } from "./styles";
