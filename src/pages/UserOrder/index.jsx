80a4292debc97cf2c2d6313c3a277e1625707ff6
balvesD3v <lpbalves7filho@gmail.com>
2023-12-18 19:22:42 -0300

    .

--- src/pages/UserOrder/index.jsx
+++ src/pages/UserOrder/index.jsx
@@ -1,5 +1,5 @@
 import { Header } from "../../components/Header";
-import { Footer } from "../../components/Footer";
+import { Footer } from "../../components/Footer/index";
 import { Tags } from "../../components/Tags";
 import {
   DivContent,
